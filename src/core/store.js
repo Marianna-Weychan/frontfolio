b5a46dc69e560e7765a6deb0a6d71a91ee881494
Marianna Weychan <mariannaweychan@gmail.com>
2023-05-24 11:49:28 +0200

    fetchRepositoriesAPIReducer added

--- src/core/store.js
+++ src/core/store.js
@@ -1,15 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "@redux-saga/core";
 import rootSaga from "./rootSaga";
-import themeReducer from "../common/themeSlice"
-
+import themeReducer from "../common/themeSlice";
+import fetchRepositoriesAPIReducer from "../features/PersonalHomepage/fetchRepositoriesSlice";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
     reducer: {
         theme: themeReducer,
-
+        myRepositories: fetchRepositoriesAPIReducer,
     },
 
     middleware: [sagaMiddleware],
