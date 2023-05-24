d7a47d6c9e777b7e4def3686351d1e8d6b8c4c01
Marianna Weychan <mariannaweychan@gmail.com>
2023-05-24 10:31:05 +0200

    GlobalStyle created

---
+++ src/core/GlobalStyle.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+    box-sizing: border-box;
+    }
+    *, ::after, ::before {
+    box-sizing: inherit;
+    }
+
+    body {
+        font-family: 'Inter', sans-serif;
+        background: ${({ theme }) => theme.colors.site.background};
+        color: ${({ theme }) => theme.colors.site.text};
+        font-size: 18px;
+        letter-spacing: 0.05em;
+        word-break: break-word;
+        transition: background 0.3s;
+        padding-bottom: 108px;
+        @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+            padding-bottom: 32px;
+        }
+            }
+`;
