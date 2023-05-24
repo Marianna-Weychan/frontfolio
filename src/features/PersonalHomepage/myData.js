3d5947fafbd794612e95e947edd63b56b4d18620
Marianna Weychan <mariannaweychan@gmail.com>
2023-05-24 17:54:38 +0200

    myData deleted

--- src/features/PersonalHomepage/myData.js
+++
@@ -1,5 +0,0 @@
-export const email = "mariannaweychan@gmail.com";
-export const facebook = "https://www.facebook.com/marianna.weychan/";
-export const github = "https://github.com/Marianna-Weychan";
-export const linkedin = "https://www.linkedin.com/in/marianna-weychan/";
-export const instagram = "https://www.instagram.com/mariannaweychan/";
