3748bffe2e0d19756df38649cbec88c0e4b0f7d7
Marianna Weychan <mariannaweychan@gmail.com>
2023-05-24 11:49:06 +0200

    myRepositoriesSaga added

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -1,8 +1,6 @@
 import { all } from "redux-saga/effects";
-
+import myRepositoriesSaga from "../features/PersonalHomepage/fetchRepositoriesSaga";
 
 export default function* rootSaga() {
-    yield all([
-
-    ]);
+    yield all([myRepositoriesSaga()]);
 }
