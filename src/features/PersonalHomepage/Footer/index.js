3561479fee2e208718ec96e13c9a8d3f372aab4c
Marianna Weychan <mariannaweychan@gmail.com>
2023-05-25 17:40:29 +0200

    constant "email" was used and hard spaces were inserted

--- src/features/PersonalHomepage/Footer/index.js
+++ src/features/PersonalHomepage/Footer/index.js
@@ -1,15 +1,16 @@
-import { Contact, Wrapper, FooterContent, LetsTalk, Address, EmailWrapper } from "./styled";
+import { ContactLink, Wrapper, FooterContent, LetsTalk, Address, EmailWrapper } from "./styled";
 import { SocialIcons } from "./SocialIcons";
+import { email } from "../email";
 
 export const Footer = () => (
     <Wrapper>
         <LetsTalk>Let's talk!</LetsTalk>
         <Address>
             <EmailWrapper>
-                <Contact href="mailto:mariannaweychan@gmail.com">mariannaweychan@gmail.com</Contact>
+                <ContactLink href={`mailto:${email}`} title={email} rel="noopener noreferrer">{email}</ContactLink>
             </EmailWrapper>
             <FooterContent>
-                I’m always open to new projects whenever I have the time. If you have a website, dashboard or mobile app in mind and need some help to make your ideas come to life, feel free to conatct me🤞
+                I’m always open to new projects whenever I have the time. If you have a&nbsp;website, dashboard or mobile app in mind and need some help to&nbsp;make your ideas come to life, feel free to conatct me🤞
             </FooterContent>
             <SocialIcons />
         </Address>
