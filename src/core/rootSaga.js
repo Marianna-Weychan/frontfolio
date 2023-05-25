92fefae1fe0fe0d26fed5d697c9b15e42a6847d5
Marianna Weychan <mariannaweychan@gmail.com>
2023-05-25 17:36:06 +0200

    changed names rootSaga to saga

--- src/core/rootSaga.js
+++
@@ -1,6 +0,0 @@
-import { all } from "redux-saga/effects";
-import { myRepositoriesSaga } from "../features/PersonalHomepage/fetchRepositoriesSaga";
-
-export default function* rootSaga() {
-    yield all([myRepositoriesSaga()]);
-}
